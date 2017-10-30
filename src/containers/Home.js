6ec401f7c4ab12598c4a7fedd9f27b9b84395207
Dino Scheidt <github@dino-scheidt.de>
2017-10-30 17:14:18 +0100

    💄 Ant Design implementation for testing and illustration

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { getSiteProps } from 'react-static'
+import { Card } from "antd";
 //
 import logoImg from '../logo.png'
 
 export default getSiteProps(() => (
   <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
-    <img src={logoImg} alt="" />
+    <Card title="Welcome to" style={{ width: "100%" }} bodyStyle={{ padding: 0 }}>
+      <div className="custom-image">
+        <img alt="react-static" width="100%" src={logoImg} />
+      </div>
+    </Card>
   </div>
 ))
