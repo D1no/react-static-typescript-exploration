d424a461b323561bdf7a43338548de39d0531b44
Dino Scheidt <github@dino-scheidt.de>
2017-10-30 13:56:58 +0100

    ✨ Add less support with andt-design

--- static.config.js
+++ static.config.js
@@ -2,8 +2,25 @@ import axios from 'axios'
 import React, { Component } from 'react'
 import { ServerStyleSheet } from 'styled-components'
 
+/*
+* For TypeScript Support
+* */
 const typescriptWebpackPaths = require("./webpack.config.js");
 
+/*
+* For Less Support
+* */
+import autoprefixer from 'autoprefixer';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import postcssFlexbugsFixes from 'postcss-flexbugs-fixes';
+
+const path = require('path');
+const fs = require('fs');
+
+const lessToJs = require('less-vars-to-js');
+const themeVariables = lessToJs(fs.readFileSync(path.join(__dirname, 'src/theme-ant-overwrite.less'), 'utf8'));
+
+//
 export default {
   getSiteProps: () => ({
     title: 'React Static',
@@ -66,8 +83,12 @@ export default {
   webpack: (config, args) => {
 
     // For Debug: Set to true to take a look at the final config.
-    const printWebpackConfigDuringBuild = false;
+    const printWebpackConfigDuringBuild = true;
     const { stage } = args; // is dev or prod
+
+    /*
+    * TypeScript Support
+    * */
 
     // Add .ts and .tsx extension to resolver
     config.resolve.extensions.push(".ts", ".tsx");
@@ -94,9 +115,148 @@ export default {
       ]
     };
 
-    // Whitelist ts(x) extensions from the universal url-loader, by overwriting
-    // the default exclude REGEX in static-react/src/webpack/rules
-    config.module.rules[2].exclude = /\.(js|jsx|css|ts|tsx)(\?.*)?$/ ;
+
+    /*
+    * Less Support
+    * */
+
+    // Add .less & .css to resolver
+    config.resolve.extensions.push(".less");
+    config.resolve.extensions.push(".css");
+
+    // Loader depending on stage
+    let lessConfig =  {};
+
+    if (stage === 'dev') {
+
+      lessConfig =
+        {
+          test: /\.less$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                minimize: false,
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                // Necessary for external CSS imports to work
+                // https://github.com/facebookincubator/create-react-app/issues/2677
+                sourceMap: true,
+                ident: 'postcss',
+                plugins: () => [
+                  postcssFlexbugsFixes,
+                  autoprefixer({
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9', // React doesn't support IE8 anyway
+                    ],
+                    flexbox: 'no-2009',
+                  }),
+                ],
+              },
+            },
+            {
+              loader: "less-loader",
+              options: {
+                sourceMap: true,
+                modifyVars: themeVariables
+              }
+            }
+          ],
+        }
+
+    } else {
+
+    lessConfig =
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: {
+            loader: 'style-loader',
+            options: {
+              hmr: false,
+              sourceMap: false
+            },
+          },
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                minimize: true,
+                sourceMap: false,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                // Necessary for external CSS imports to work
+                // https://github.com/facebookincubator/create-react-app/issues/2677
+                ident: 'postcss',
+                plugins: () => [
+                  postcssFlexbugsFixes,
+                  autoprefixer({
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9', // React doesn't support IE8 anyway
+                    ],
+                    flexbox: 'no-2009',
+                  }),
+                ],
+              },
+            },
+            {
+              loader: "less-loader",
+              options: {
+                sourceMap: false,
+                modifyVars: themeVariables,
+                paths: [
+                  path.resolve(__dirname, "node_modules/andt/lib/style")
+                ]
+              }
+            }
+          ],
+        }),
+      }
+    }
+
+    // Add less config to rules
+    config.module.rules.push(lessConfig);
+
+    // Update ExtractTextPlugin with current instance
+    config.plugins[2] =
+      new ExtractTextPlugin({
+        filename: getPath => {
+          process.env.extractedCSSpath = 'styles.css';
+          return getPath('styles.css')
+        },
+        allChunks: true,
+      });
+
+    /*
+    * In the future, this segment will change or be unnecessary.
+    * Whitelisting Extensions from pre-parser and universal url-loader
+    * */
+
+    // Tell pre-processor (babel) to ignore styles since, though they are
+    // imported as modules, are not parse able code.
+    require("babel-register");
+    require.extensions[".less"] = () => {};
+    require.extensions[".css"] = () => {};
+
+    // Whitelist less and ts(x) extensions from the universal url-loader, by
+    // overwriting the default exclude REGEX in static-react/src/webpack/rules
+    config.module.rules[2].exclude = /\.(js|jsx|css|less|ts|tsx)(\?.*)?$/ ;
 
     // For Debugging and inspection. Final Webpack Config used during build.
     if(printWebpackConfigDuringBuild && stage === "dev") {
